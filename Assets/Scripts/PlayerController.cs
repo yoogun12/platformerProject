86adb5d2ccfd70afbe511aad903e879796386c72
yoogun12 <yoogun1313@gmail.com>
2025-04-09 14:22:12 +0900

    {feat}점프 애니메이션

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,11 +13,13 @@ public class PlayerController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
+    private Animator pAni;
     private bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
     }
 
     private void Update()
@@ -25,11 +27,18 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
+        if (moveInput < 0)
+            transform.localScale = new Vector3(1f, 1f, 1f);
+
+        if (moveInput > 0)
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
         if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("JumpAction");
         }
     }
         private void OnTriggerEnter2D(Collider2D collision)
