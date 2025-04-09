40f9a349868a53e55c452677aa777874ecf562ff
yoogun12 <yoogun1313@gmail.com>
2025-04-09 16:27:11 +0900

    {Feat} 거인화

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     private Animator pAni;
     private bool isGrounded;
 
+    private bool isGiant = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,11 +29,22 @@ public class PlayerController : MonoBehaviour
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-        if (moveInput < 0)
-            transform.localScale = new Vector3(1f, 1f, 1f);
+        if (isGiant)
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(2f, 2f, 1f);
 
-        if (moveInput > 0)
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-2f, 2f, 1f);
+        }
+        else
+        {
+            if (moveInput < 0)
+                transform.localScale = new Vector3(1f, 1f, 1f);
+
+            if (moveInput > 0)
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
@@ -41,8 +54,8 @@ public class PlayerController : MonoBehaviour
             pAni.SetTrigger("JumpAction");
         }
     }
-        private void OnTriggerEnter2D(Collider2D collision)
-        {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         if (collision.CompareTag("Respawn"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -55,7 +68,16 @@ public class PlayerController : MonoBehaviour
 
         if (collision.CompareTag("Enemy"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if (isGiant)
+                Destroy(collision.gameObject);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (collision.CompareTag("Item"))
+        {
+            isGiant = true;
+            Destroy(collision.gameObject);
         }
     }
 }
